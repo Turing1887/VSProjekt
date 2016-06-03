d75bb3d168c8ab546c76adc0833172f657b80843
Turing1887 <tim.zschage@t-online.de>
2016-06-03 21:24:18 +0200

    Upload Glow.cs/Update Projektidee

---
+++ Skripte/Glow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glow : MonoBehaviour {
+
+	Material mat;
+	Color currCol;
+	Color newCol;
+	Renderer rend;
+	// Use this for initialization
+	void Awake () {
+		rend = GetComponent<Renderer> ();
+		mat = rend.material;
+
+	}
+
+	void FixedUpdate(){
+		NotHit ();
+	}
+
+	void HitByRay(){
+		
+		mat.EnableKeyword ("_EMISSION");
+
+	}
+
+	void NotHit(){
+		mat.DisableKeyword ("_EMISSION");
+	}
+
+
+
+}
