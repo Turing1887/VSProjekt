6ab9af5c0f3618adcfdc4fc67346187ac9b868dd
Turing1887 <tim.zschage@t-online.de>
2016-06-06 18:33:57 +0200

    Update Projektidee/Boxmensch

---
+++ Skripte/CustomerSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerSpawn : MonoBehaviour {
+
+
+	public GameObject customer;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn",Random.Range(4f,10f),Random.Range(4f,10f));
+	}
+
+
+	void Spawn(){
+		Vector3 spawnPoint = transform.position;
+		Instantiate (customer,spawnPoint,Quaternion.identity);
+
+	}	
+}
